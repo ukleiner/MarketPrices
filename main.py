7f192c1f8f9783ffa53e85c7f38d5a5a27c75352
uri <ukleiner@gmail.com>
2023-06-07 17:35:48 +0300

    Basic data, not functional yet

---
+++ main.py
@@ -0,0 +1,17 @@
+# A program to collect products price data from supermarkets
+'''
+    ---------------------
+    Parameters:
+    Uses:
+    =====================
+    Return:
+    Side effects:
+'''
+http://prices.shufersal.co.il/ # for shufersal, no passcode
+http://matrixcatalog.co.il/NBCompetitionRegulations.aspx # victory, no passcode
+http://publishprice.ybitan.co.il/ # yenot bitan, no passcode
+https://url.retail.publishedprices.co.il/login # TivTaam, yohananof, osherad,
+# Keshet Teamim, RamiLevi
+
+# Categories
+Price, PriceFull, Promo, PromoFull, Stores
