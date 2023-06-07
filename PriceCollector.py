3022dc2e9a6890741584ee3297e189dc790549df
uri <ukleiner@gmail.com>
2023-06-07 17:36:03 +0300

    basic APIs for stores to implement

---
+++ PriceCollector.py
@@ -0,0 +1,5 @@
+class PriceCollector:
+    def __init__(self):
+        pass
+    def parse(self, fn):
+        pass
---
+++ Store.py
@@ -0,0 +1,8 @@
+class Store:
+    '''
+    The basic functions each store should implement
+    '''
+    def __init__(self, url, username, password):
+        pass
+    def get():
+        pass
