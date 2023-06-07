2f9fff69ae1e890d8e00fce8cb541f5a0dda4982
uri <ukleiner@gmail.com>
2023-06-07 17:39:07 +0300

    Basic structure of Item

---
+++ Item.py
@@ -0,0 +1,14 @@
+class Item:
+    def __init__(self, xmlObject):
+        self.obj = xmlObject
+        self._parse()
+
+    def _parse(self):
+        # PriceUpdateDate
+        # ItemName
+        # ManufacturerName
+        # ManufactureCountry
+        # UnitOfMeasure
+        # UnitOfMeasurePrice
+        pass
+
