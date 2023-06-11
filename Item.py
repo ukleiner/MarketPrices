235b14a2845666124032e1be2fa1b5e2e5c1e868
uri <ukleiner@gmail.com>
2023-06-11 13:32:18 +0300

    Basic extraction of details of interest

--- Item.py
+++ Item.py
@@ -10,5 +10,18 @@ class Item:
         # ManufactureCountry
         # UnitOfMeasure
         # UnitOfMeasurePrice
-        pass
+#      <PriceUpdateDate>2022-03-27 15:09</PriceUpdateDate>
+        for elem in self.obj.iter():
+            if elem.tag == 'ItemCode':
+                self.code = elem.text
+            elif elem.tag == 'ItemName':
+                self.name = elem.text
+            elif elem.tag == 'ManufacturerName':
+                self.manu = elem.text
+            elif elem.tag == 'UnitOfMeasure':
+                self.units = elem.text
+            elif elem.tag == 'UnitOfMeasurePrice':
+                self.price = elem.text
+            elif elem.tag == 'PriceUpdateDate':
+                self.update_date = elem.text
 
