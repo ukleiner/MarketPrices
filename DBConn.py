1141cd2ab62fed7b785961542e82b9b6df8d5cb8
uri <ukleiner@gmail.com>
2023-06-11 14:21:26 +0300

    Created database class that can create the DB structure

---
+++ DBConn.py
@@ -0,0 +1,104 @@
+import sqlite3
+
+class DB:
+    def __init__(self, store):
+        self.store = store
+        self.con = sqlite3.connect('agriItems.db')
+        self.cur = con.cursor()
+
+    def logPrice(self, chain, store, items):
+        query = '''INSERT INTO price (`item`, `update_date`, `price`)
+        VALUES(?)'''
+        cur = con.cursor()
+        cur.executemany(query, [(token,) for token in tokens])
+        con.commit()
+        cur.close()
+        pass
+
+    def dbStruct(self):
+        self.createChains()
+        self.createSubchains()
+        self.createStores()
+        self.createStoreItems()
+        self.createPrices()
+        self.createItems()
+        self.createItemLinker()
+
+    def createChains(self):
+        query = '''CREATE TABLE chain (
+        id INTEGER PRIMARY KEY,
+        Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+        chainId INTEGER,
+        chainName TEXT
+        '''
+        self.cur.execute(query)
+
+    def createSubchains(self):
+        '''
+        type is a column I added to describe what type of chain it is
+        '''
+        query = '''CREATE TABLE subchain (
+        id INTEGER PRIMARY KEY,
+        Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+        chain INTEGER NOT NULL,
+        subchainId INTEGER,
+        name TEXT,
+        type INTEGER,
+        FOREIGN KEY(chain) REFERENCES chain(id)
+        '''
+        self.cur.execute(query)
+
+    def createStores(self):
+        query = '''CREATE TABLE store (
+        id INTEGER PRIMARY KEY,
+        Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+        subcain INTEGER NOT NULL,
+        store INTEGER NOT NULL,
+        name TEXT NOT NULL,
+        city TEXT,
+        FOREIGN KEY(subchain) REFERENCES subchain(id)
+        )'''
+        self.cur.execute(query)
+
+    def createStoreItems(self):
+        query = '''CREATE TABLE storeItem (
+        id INTEGER PRIMARY KEY,
+        Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+        store INTEGER NOT NULL,
+        code INTEGER NOT NULL,
+        name TEXT NOT NULL,
+        manufacturer TEXT NOT NULL,
+        units TEXT,
+        FOREIGN KEY(store) REFERENCES store(id)
+        )'''
+        self.cur.execute(query)
+
+    def createPrices(self):
+        query = '''CREATE TABLE price (
+        id INTEGER PRIMARY KEY,
+        Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+        item INTEGER NOT NULL,
+        update_date DATETIME NOT NULL,
+        price REAL,
+        FOREIGN KEY(item) REFERENCES item(id)
+        )'''
+        self.cur.execute(query)
+
+    def createItems(self):
+        query = '''CREATE TABLE item(
+        id INTEGER PRIMARY KEY,
+        Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+        item_name INTEGER NOT NULL
+        )'''
+        self.cur.execute(query)
+
+    def createItemLinker(self):
+        query = '''CREATE TABLE item_link(
+        id INTEGER PRIMARY KEY,
+        item INTEGER NOT NULL,
+        storeItem INTEGER NOT NULL,
+        FOREIGN KEY(item) REFERENCES item(id)
+        FOREIGN KEY(storeItem) REFERENCES storeItem(id)
+        )'''
+        self.cur.execute(query)
+
