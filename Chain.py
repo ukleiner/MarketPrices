18211e012eed5c8425872179b29d7167ecc80d61
uri <ukleiner@gmail.com>
2023-06-12 07:30:26 +0300

    getting stores and preparing lists for insertion

--- Chain.py
+++ Chain.py
@@ -46,6 +46,20 @@ class Chain:
         cur.execute(query, (chain, subchain, name))
         return cur.lastrowid
 
+    def getStores(self, chain):
+        '''
+            Fetch storeinternal Id conversion from chain Id
+            ---------------------
+            Parameters:
+                chain - internal chain id
+            =====================
+            Return:
+            dict store (external) to store (internal)
+        '''
+        cur = self.db.getCursor()
+        query = "SELECT id, store FROM store WHERE chain = ?"
+        cur.execute(query, (chain,))
+        return({store.store: store.id for store in cur.fetchall()})
 
     def obtainStores(fn):
         '''
@@ -65,14 +79,23 @@ class Chain:
             chain = self.insertChain(chainId)
 
         subchains = self.getSubchains(chain)
+        stores = self.getStores(chain)
 
         storesElem = context.find('STORES')
-        stores = []
-        for storein storesElem.iter():
+        storesIns = {}
+        for store in storesElem.iter():
             subchainId = store.find('SUBCHAINID')
             if subchainId not in subchains:
                 scname = store.find('SUBCHAINNAME')
                 subchain = self.createSubchain(chain, subchainId, name)
                 subchains[subchainId] = subchain
+            subchain = subchains[subchainId]
+            store = store.find("STOREID")
+            storeName = store.find("STORENAME")
+            city = store.find("CITY")
+            stores[store] = [chain, store, storeName, city]
+            storeLinks[store] = [subchain, store]
+
+
 
 
