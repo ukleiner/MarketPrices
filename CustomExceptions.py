773b82c9be6eab65a9d4e8e5d5f03655b811f50d
uri <ukleiner@gmail.com>
2023-06-13 18:51:54 +0300

    Added explanation to exceptions

--- CustomExceptions.py
+++ CustomExceptions.py
@@ -1,11 +1,14 @@
+'''Raised when a a file of a different chain is used'''
 class WrongChainFileException(Exception):
     def __init__(self):
         pass
 
+'''Raised when a prices file has a store not in the db'''
 class NoStoreException(Exception):
     def __init__(self):
         pass
 
+'''Raised when a store is missing from the most up-to-date stores file'''
 class NoSuchStoreException(Exception):
     def __init__(self):
         pass
