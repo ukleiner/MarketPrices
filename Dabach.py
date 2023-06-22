d491af9967d88347f2387a76b87316a13ba7fcc1
uri <ukleiner@gmail.com>
2023-06-22 08:52:42 +0300

    Added Dabach

--- Dabach.py
+++ Dabach.py
@@ -2,15 +2,15 @@ import re
 
 from CerberusChain import CerberusChain
 
-class TivTaam(CerberusChain):
+class Dabach(CerberusChain):
     '''
     The basic functions each Chain should implement
     '''
     def __init__(self, db):
         url = "https://url.retail.publishedprices.co.il"
-        username = "TivTaam"
-        password = ''
-        name = 'TivTaam'
-        chainId = 7290873255550
-        codeCategoryR = re.compile(r'>\d{1,7}<')
+        username = "SalachD"
+        password = '12345'
+        name = 'Dabach'
+        chainId = 7290526500006
+        codeCategoryR = re.compile("729000000")
         super().__init__(db, url, username, password, name, chainId, codeCategoryR=codeCategoryR)
