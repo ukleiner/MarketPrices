e466fea6e101cf2d0bc625fad780f44912d0d44f
uri <ukleiner@gmail.com>
2023-06-25 19:00:41 +0300

    Added adaptations for MatrixChain

--- Item.py
+++ Item.py
@@ -16,11 +16,14 @@ class Item:
         for elem in obj.iter():
             if elem.tag == 'ItemCode':
                 self.code = int(elem.text)
+            # ItemNm for KingStore
             elif elem.tag in  ['ItemName', 'ItemNm']:
                 self.name = elem.text
-            elif elem.tag == 'ManufacturerName':
+            # ManufactureName for MatrixChain
+            elif elem.tag in ['ManufacturerName', 'ManufactureName']:
                 self.manu = elem.text
-            elif elem.tag == 'UnitOfMeasure':
+            # UnitQty for MatrixChain
+            elif elem.tag in ['UnitOfMeasure', 'UnitQty']:
                 self.units = elem.text
             elif elem.tag == 'UnitOfMeasurePrice':
                 self.price = elem.text
